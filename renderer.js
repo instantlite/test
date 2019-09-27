f261c1c7bf12bb3b9a65986524b532f600fd4d97
Dalton Menezes <daltonmenezes@outlook.com>
2019-09-27 01:51:19 -0300

    new v2

--- renderer.js
+++ renderer.js
@@ -1,7 +1,3 @@
-const ms = require('ms')
-
 module.exports = () => {
-  console.log('New test plugin is running, see: ' + ms(5311340000))
-  console.log('Whoooopy, I love 🍕')
-  console.log('Yey 🍕🍕🍕🍕')
+  console.log('Looking for a new version 🔍 🔍 🔍 🔍 ')
 }
